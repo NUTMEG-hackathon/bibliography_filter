0a9d0240c64695023e7b1fc62210f7603e8db50a
Toshiki Imai <tosiki_801_life@outlook.jp>
2022-06-16 22:15:48 +0900

    fix setting, pages can show flaxapi reactions

--- api/custom_search.py
+++ api/custom_search.py
@@ -1,8 +1,23 @@
 import os
 from dotenv import load_dotenv
+from fastapi import FastAPI
+from pydantic import BaseModel
+from starlette.middleware.cors import CORSMiddleware
 
 # .envファイルの内容を読み込みます
 load_dotenv()
+
+app = FastAPI()
+origins = [
+    "http://localhost:8000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 import json                                 # 返却された検索結果の読み取りにつかう
 from googleapiclient.discovery import build # APIへのアクセスにつかう
@@ -15,55 +30,56 @@ API_KEY = (os.environ['API_KEY'])
 
 # APIにアクセスして結果をもらってくるメソッド
 def get_search_results(query):
-   
-   # APIでやりとりするためのリソースを構築
-   # 詳細: https://googleapis.github.io/google-api-python-client/docs/epy/googleapiclient.discovery-pysrc.html#build
-   search = build(
-       "customsearch", 
-       "v1", 
-       developerKey = API_KEY
-   )
-   
-   # Google Custom Search から結果を取得
-   # 詳細: https://developers.google.com/custom-search/v1/reference/rest/v1/cse/list
-   result = search.cse().list(
-       q = query,
-       cx = CUSTOM_SEARCH_ENGINE_ID,
-       lr = 'lang_ja',
-       num = 10,
-       start = 1
-   ).execute()
+    
+    # APIでやりとりするためのリソースを構築
+    # 詳細: https://googleapis.github.io/google-api-python-client/docs/epy/googleapiclient.discovery-pysrc.html#build
+    search = build(
+        "customsearch", 
+        "v1", 
+        developerKey = API_KEY
+    )
+    
+    # Google Custom Search から結果を取得
+    # 詳細: https://developers.google.com/custom-search/v1/reference/rest/v1/cse/list
+    result = search.cse().list(
+        q = query,
+        cx = CUSTOM_SEARCH_ENGINE_ID,
+        lr = 'lang_ja',
+        #    num = 10,
+        num = 1,
+        start = 1
+    ).execute()
 
-   # 受け取ったjsonをそのまま返却
-   return result
+    # 受け取ったjsonをそのまま返却
+    return result
 
 
 # 検索結果の情報をSearchResultに格納してリストで返す
 def summarize_search_results(result):
 
-   # 結果のjsonから検索結果の部分を取り出しておく
-   result_items_part = result['items']
+    # 結果のjsonから検索結果の部分を取り出しておく
+    result_items_part = result['items']
 
-   # 抽出した検索結果の情報はこのリストにまとめる
-   result_items = []
-   
-   # 今回は (start =) 1 個目の結果から (num =) 10 個の結果を取得した
-   for i in range(0, 10):
-       # i番目の検索結果の部分
-       result_item = result_items_part[i]
-       # i番目の検索結果からそれぞれの属性の情報をResultクラスに格納して
-       # result_items リストに追加する
-       result_items.append(
-           SearchResult(
-               title = result_item['title'],
-               url = result_item['link'],
-               snippet = result_item['snippet'],
-               rank = i + 1
-           )
-       )
+    # 抽出した検索結果の情報はこのリストにまとめる
+    result_items = []
+    
+    # 今回は (start =) 1 個目の結果から (num =) 10 個の結果を取得した
+    for i in range(0, 10):
+        # i番目の検索結果の部分
+        result_item = result_items_part[i]
+        # i番目の検索結果からそれぞれの属性の情報をResultクラスに格納して
+        # result_items リストに追加する
+        result_items.append(
+            SearchResult(
+                title = result_item['title'],
+                url = result_item['link'],
+                snippet = result_item['snippet'],
+                rank = i + 1
+            )
+        )
 
-   # 結果を格納したリストを返却
-   return result_items
+    # 結果を格納したリストを返却
+    return result_items
 
        
 # 検索結果の情報を格納するクラス
@@ -78,6 +94,17 @@ class SearchResult:
         # コマンドライン上での表示形式はご自由にどうぞ
         return "[title] " + self.title + "\n\t[url] " + self.url + "\n\t[snippet] " + self.snippet + "\n\t[rank] " + str(self.rank)
 
+class testParam(BaseModel):
+    search : str
+
+@app.get("/")
+def get_root():
+    return {"message": "Search sample"}
+
+@app.post("/")
+def post_root():
+    print(testParam)
+    return testParam
 
 # メインプロセス       
 if __name__ == '__main__':
